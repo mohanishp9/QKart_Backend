7cd60ed9bad3fb39910d47c047baf5b50857d8e0
Mohanish Pingale <mpingale31@gmail.com>
2024-10-25 14:33:57 +0530

    QKart_Backend_First_Commit

---
+++ src/routes/v1/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const userRoute = require("./user.route");
+const authRoute = require("./auth.route");
+const productRoute = require("./product.route");
+
+const router = express.Router();
+
+router.use("/users", userRoute);
+// TODO: CRIO_TASK_MODULE_AUTH - Reroute all API requests beginning with the `/v1/auth` route to Express router in auth.route.js 
+router.use("/products", productRoute);
+
+module.exports = router;
---
+++ src/routes/v1/user.route.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const validate = require("../../middlewares/validate");
+const userValidation = require("../../validations/user.validation");
+const userController = require("../../controllers/user.controller");
+const auth = require("../../middlewares/auth");
+
+const router = express.Router();
+
+router.get(
+  "/:userId",
+  validate(userValidation.getUser),
+  userController.getUser
+);
+
+module.exports = router;
